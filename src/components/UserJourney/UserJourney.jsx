a87ea8af2062a506a8b8e13ba89daf4de0c1a2f2
Nick <nikitaromanov60@gmail.com>
2025-10-12 20:50:53 +0300

    Minor UserJourney update

--- src/components/UserJourney/UserJourney.jsx
+++ src/components/UserJourney/UserJourney.jsx
@@ -2,11 +2,11 @@ import "./UserJourney.scss"
 
 export default function UserJourney({id, text}) {
     return(
-        <span className="user-container">
-            <span className="userId">
+        <span className="user__container">
+            <span className="user__Id">
                 <h2>{id}</h2>
             </span>
-            <span className="stepText">
+            <span className="user__stepText">
                 <p className="body1">{text}</p>
             </span>
         </span>
