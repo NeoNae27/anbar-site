9243a2aaf4789bc3fadc470c643a62f88c9d124b
Nick <nikitaromanov60@gmail.com>
2025-10-15 20:42:03 +0300

    Add google map to Contacts

--- src/layout/Contacts/Contacts.jsx
+++ src/layout/Contacts/Contacts.jsx
@@ -8,13 +8,15 @@ export default function Contacts() {
     <section className="contacts">
       <div className="contacts__container">
         <div className="contacts__map">
-          <Image
-            src="/imgs/map.png"
-            alt="map"
-            width={475}
-            height={413}
-            unoptimized
-          />
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3355.935811621989!2d49.760321!3d40.299592!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDE3JzU4LjUiTiA0OcKwNDUnMzcuMiJF!5e1!3m2!1sen!2sru!4v1760549307871!5m2!1sen!2sru"
+            width="100%"
+            
+            height="413"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
         <div className="contacts__info">
           <div className="contacts__address">
