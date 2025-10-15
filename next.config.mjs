4c612bdb2408acfdf06a5060a3119a589f3bb08b
Neonae <76795335+NeoNae27@users.noreply.github.com>
2025-10-15 23:56:47 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'anbar.al-site'; // 
-
 const nextConfig = {
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   },
