47e95b818c58696f278698a4f7315f5eafbf04d3
Nick <nikitaromanov60@gmail.com>
2025-11-03 05:01:09 +0300

    Update ContainerSpecs

--- src/layout/ContainerSpecs/ContainerSpecs.jsx
+++ src/layout/ContainerSpecs/ContainerSpecs.jsx
@@ -3,40 +3,40 @@ import SmallCard from "@/components/Cards/SmallCard/SmallCard";
 import "./ContainerSpecs.scss";
 
 export default function ContainerSpecs() {
-  const ContainerSpecList = [
-    {
-      icon: "/icons/container/camera.svg",
-      alt: "Camera icon",
-      ariaLabel: "24/7 \n Mühafizə icon",
-      title: `24/7 \n Mühafizə`,
-      description:
-        "Konteynerlərin hər biri daimi təhlükəsizlik kameraları və mühafizə ilə müşahidə olunur",
-    },
-    {
-      icon: "/icons/container/fire.svg",
-      alt: "Map icon",
-      ariaLabel: "Yanğın Təhlükəsizliyi icon",
-      title: "Yanğın Təhlükəsizliyi",
-      description:
-        "Konteynerlərin hər biri yanğın detektoru ilə təchiz olunub",
-    },
-    {
-      icon: "/icons/container/bulb.svg",
-      alt: "Bulb icon",
-      ariaLabel: "İşıqlandırma və Elektrik Təchizatı icon",
-      title: "İşıqlandırma və Elektrik Təchizatı",
-      description:
-        "Hər konteynerin öz işıqlandırması və elektrik yuvası mövcuddur",
-    },
-    {
-      icon: "/icons/container/rain.svg",
-      alt: "Rain icon",
-      ariaLabel: "Nəmişliyə və Temperaturaya Qarşı İzolyasiya icon",
-      title: "Nəmişliyə və Temperaturaya Qarşı İzolyasiya",
-      description:
-        "Konteynerlər mallarınıza zərər gəlməsin üçün izolyasiya olunub",
-    },
-  ];
+  // const ContainerSpecList = [
+  //   {
+  //     icon: "/icons/container/camera.svg",
+  //     alt: "Camera icon",
+  //     ariaLabel: "24/7 \n Mühafizə icon",
+  //     title: `24/7 \n Mühafizə`,
+  //     description:
+  //       "Konteynerlərin hər biri daimi təhlükəsizlik kameraları və mühafizə ilə müşahidə olunur",
+  //   },
+  //   {
+  //     icon: "/icons/container/fire.svg",
+  //     alt: "Map icon",
+  //     ariaLabel: "Yanğın Təhlükəsizliyi icon",
+  //     title: "Yanğın Təhlükəsizliyi",
+  //     description:
+  //       "Konteynerlərin hər biri yanğın detektoru ilə təchiz olunub",
+  //   },
+  //   {
+  //     icon: "/icons/container/bulb.svg",
+  //     alt: "Bulb icon",
+  //     ariaLabel: "İşıqlandırma və Elektrik Təchizatı icon",
+  //     title: "İşıqlandırma və Elektrik Təchizatı",
+  //     description:
+  //       "Hər konteynerin öz işıqlandırması və elektrik yuvası mövcuddur",
+  //   },
+  //   {
+  //     icon: "/icons/container/rain.svg",
+  //     alt: "Rain icon",
+  //     ariaLabel: "Nəmişliyə və Temperaturaya Qarşı İzolyasiya icon",
+  //     title: "Nəmişliyə və Temperaturaya Qarşı İzolyasiya",
+  //     description:
+  //       "Konteynerlər mallarınıza zərər gəlməsin üçün izolyasiya olunub",
+  //   },
+  // ];
 
   return (
     <section
@@ -63,7 +63,7 @@ export default function ContainerSpecs() {
             </p>
           </header>
 
-          <span className="CS__cards">
+          {/* <span className="CS__cards">
             {ContainerSpecList.map((spec) => (
               <SmallCard
                 key={spec.title}
@@ -72,7 +72,7 @@ export default function ContainerSpecs() {
                 description={spec.description}
               ></SmallCard>
             ))}
-          </span>
+          </span> */}
         </span>
       </span>
     </section>
